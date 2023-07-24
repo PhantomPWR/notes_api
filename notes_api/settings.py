543893fa1380259e97b3b8e6b2cbc1374340f1c1
Jean de Villiers <jeandevilliers@me.com>
2023-07-24 18:05:09 +0100

    Finish API Tutorial

--- notes_api/settings.py
+++ notes_api/settings.py
@@ -55,9 +55,11 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+# ]
+
+CORS_ALLOW_ALL_ORIGINS: True
 
 ROOT_URLCONF = "notes_api.urls"
 
